45ce00032415c9998987fbed3431e233c3e05905
UMA-SHANKER-16 <umashankerdcs20@gmail.com>
2026-01-08 20:58:06 +0530

    Create 02_dataType.js

---
+++ 02_dataType.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") we using node js not browser
+
+console.log(3+3);
+console.log("chudhary")
+// readabilty more preferable
+
+
+let name="uma"
+let age=25
+let isLoggedIn=false
+
+// number =>2 to power 53
+//bigint
+//String=>""
+//boolean=>true/false
+//null => standalone value
+// undefined => value not assigned
+//symbol => unique 
+
+// object
+
+console.log(typeof null)
+// typeof krne pr ye answer dete hai
+//null => object
+// undefined => undefined typeof
